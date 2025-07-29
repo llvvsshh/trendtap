1e51a48f0686f90ffcaf2f8ed4617fcf7c5519b2
llvvsshh <lordlavish6ix@gmail.com>
2025-07-28 18:12:07 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // server.js
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 // YouTube Trending Endpoint
